064670fb444297a522aff92a218a6477b5b3743d
Denisolt <dshakhbu@nyit.edu>
2017-02-23 16:00:58 -0500

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^post_list$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^signup', views.signup, name='signup'),
+    url(r'^events', views.events, name='events'),
 
 ]
